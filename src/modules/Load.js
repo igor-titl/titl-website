ee06a58d9c6222ff854593f1ad48def6c131ce1d
nickozy <104327959+nickozy@users.noreply.github.com>
2023-06-05 01:15:20 +0300

    update

--- src/modules/Load.js
+++ src/modules/Load.js
@@ -7,17 +7,26 @@ export default class extends module {
     }
 
     init() {
-        const load = new modularLoad({
-            enterDelay: 300,
+        this.load = new modularLoad({
+            enterDelay: 500,
             transitions: {
-                customTransition: {}
             }
-        });
+        })
+        
+        this.load.on('loading', (transition, oldContainer) => {
+            console.log("destroy")
+            Webflow.destroy()
+        })
 
-        load.on('loaded', (transition, oldContainer, newContainer) => {
+        this.load.on('loaded', (transition, oldContainer, newContainer) => {
+            console.log("new lottie")
             this.call('destroy', oldContainer, 'app');
             this.call('update', newContainer, 'app');
-        });
-
+         
+        })
+        this.load.on('loaded', (transition, oldContainer, newContainer) => {
+            Webflow.ready()
+            Webflow.require('ix2').init()
+        })
     }
 }
