edc44ec992e7958e266440ed1d979b00b9723dfe
nickozy <104327959+nickozy@users.noreply.github.com>
2023-06-04 15:56:37 +0300

    add scroll

--- src/modules/Scroll.js
+++ src/modules/Scroll.js
@@ -1,31 +1,107 @@
 import { module } from 'modujs';
-import { lazyLoadImage } from "../utils/image";
+import { lazyLoadImage } from '../utils/image';
 import LocomotiveScroll from 'locomotive-scroll';
+import { html } from '../utils/environment';
+import gsap from 'gsap'
+
+const HEADER_THRESHOLD = 300
 
 export default class extends module {
     constructor(m) {
-        super(m);
+        super(m)
+
+        if (history.scrollRestoration) {
+            history.scrollRestoration = 'manual'
+
+            window.scrollTo(0,0)
+        }
+
+        // Binding
+        this.onResizeBind = this.onResize.bind(this)
+        this.onScrollBind = this.onScroll.bind(this)
     }
 
+    ///////////////
+    // Lifecyle
+    ///////////////
     init() {
-        this.scroll = new LocomotiveScroll({
-            el: this.el,
-            getDirection: true,
-            smooth: true,
-        });
+        // Events
+        this.bindEvents()
 
-        this.scroll.on('call', (func, way, obj, id) => {
-            // Using modularJS
-            this.call(func[0], { way, obj }, func[1], func[2]);
-        });
+console.log("lenis init")
 
-        this.scroll.on('scroll', (args) => {
-            // console.log(args.scroll);
+        // Scroll Instance
+        this.locomotiveScrollInstance = new LocomotiveScroll({
+            lenisOptions:{
+                duration: 1.2,
+                easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+                direction: 'vertical', // vertical, horizontal
+                gestureDirection: 'vertical', // vertical, horizontal, both
+                smooth: true,
+                mouseMultiplier: 1,
+                smoothTouch: false,
+                touchMultiplier: 2,
+
+            },
+            triggerRootMargin: "-1px -5% -1px -5%",
+            scrollCallback: this.onScrollBind,
+            modularInstance: this,
+            initCustomTicker: (render) => {
+                gsap.ticker.add(render);
+            },
+            destroyCustomTicker: (render) => {
+                gsap.ticker.remove(render);
+            }
         })
 
-        this.scroll.on('scroll', function (t) {
-            document.documentElement.setAttribute("data-direction", t.direction);
-        });
+        this.locomotiveScrollInstance.start()
+
+        this.onScrollBind
+
+        if(html.scrollTop > HEADER_THRESHOLD) {
+            html.classList.add('has-scrolled')
+        }
+    }
+
+    destroy() {
+        // Events
+        this.unbindEvents()
+
+        // Scroll Instance
+        this.locomotiveScrollInstance?.destroy()
+    }
+
+    ///////////////
+    // Events
+    ///////////////
+    bindEvents() {
+        window.addEventListener("resize", this.onResizeBind)
+    }
+
+    unbindEvents() {
+        window.removeEventListener("resize", this.onResizeBind)
+    }
+
+    ///////////////
+    // Callbacks
+    ///////////////
+    onScroll({ scroll, limit, velocity, direction, progress }) {
+        // Show / Hide fixed header
+        if(scroll > HEADER_THRESHOLD && !html.classList.contains('has-scrolled')) {
+            html.classList.add('has-scrolled')
+        } else if (scroll <= HEADER_THRESHOLD && html.classList.contains('has-scrolled')) {
+            html.classList.remove('has-scrolled')
+        }
+
+        // Set global velocity variable
+        // used by ScalingVisual.js
+        window.scrollVelocity = velocity
+        
+        window.scrollDirection = direction
+    }
+
+    onResize() {
+        this.locomotiveScrollInstance?.resize()
     }
 
     /**
@@ -46,12 +122,50 @@ export default class extends module {
      * @param {LocomotiveScroll} args - The Locomotive Scroll instance.
      */
     lazyLoad(args) {
-        lazyLoadImage(args.obj.el, null, () => {
-            //callback
-        })
+        lazyLoadImage(args.target)
     }
 
-    destroy() {
-        this.scroll.destroy();
+    removeScrollElements($oldContainer) {
+        this.locomotiveScrollInstance?.removeScrollElements($oldContainer)
+        
+    }
+
+    addScrollElements($newContainer) {
+        this.locomotiveScrollInstance?.addScrollElements($newContainer)
+        
+        
+    }
+
+    stop() {
+        this.locomotiveScrollInstance?.stop()
+    }
+
+    start() {
+        this.locomotiveScrollInstance?.start()
+    }
+
+    /**
+     * ScrollTo
+     *
+     * @param {Int, NodeElement or String} target - The scrollTo a target
+     * @param {Object} options - The scrollTo options (offset, duration, easing, immediate)
+     *
+     * @see https://github.com/studio-freight/lenis#methods
+     *
+     */
+    scrollTo(params) {
+        const { target, options } = params
+        this.locomotiveScrollInstance?.lenisInstance?.scrollTo(target, options)
+    }
+
+    // Hide header when footer is in-view
+    hideHeader(args) {
+        if (args.way === 'enter') {
+            html.classList.add('has-header-hidden')
+        }
+        if (args.way === 'leave') {
+            html.classList.remove('has-header-hidden')
+        }
     }
 }
+
