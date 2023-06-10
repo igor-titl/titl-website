1cefb904c8846f1c9ba57bb297771ce457b0332c
nickozy <104327959+nickozy@users.noreply.github.com>
2023-06-10 23:34:41 +0300

    add style

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,50 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const target = "browserslist";
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 module.exports = {
+  target,
   mode: "production",
   devtool: "source-map",
+  plugins: [new MiniCssExtractPlugin()],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new CssMinimizerPlugin(),
+      new TerserWebpackPlugin(),
+    ],
+    minimize: true,
+  },
 };
