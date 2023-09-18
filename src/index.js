e8e25b8a4a0429d2a84c5fc2b9a5dfb9ef4d4afc
nickozy <104327959+nickozy@users.noreply.github.com>
2023-09-18 23:36:29 +0300

    1

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const app = new modular({
 });
 
 window.onload = (event) => {
+  document.querySelector("#app").remove();
+  document.querySelector("#app-style").remove();
+
   const $style = document.getElementById("main-css");
 
   if ($style) {
