2a0f08043b12bdb8c50a350c29f49432ce335e28
nickozy <104327959+nickozy@users.noreply.github.com>
2023-09-27 23:31:09 +0300

    1

--- src/index.js
+++ src/index.js
@@ -12,17 +12,6 @@ const app = new modular({
 window.onload = (event) => {
   const $style = document.getElementById("main-css");
 
-  document
-    .querySelector(
-      '[src="https://assets.website-files.com/64414ea18a1b2e622a6cce39/js/webflow.4afac6d39.js"]'
-    )
-    .remove();
-  document
-    .querySelector(
-      '[src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=64414ea18a1b2e622a6cce39"]'
-    )
-    .remove();
-
   // document.querySelector("#css").remove();
   // document.querySelector("#app").remove();
 
