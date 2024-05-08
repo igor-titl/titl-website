05ecf4b6921c6adfe379f0063801b00bc2aff83a
mashiro228 <igor.klochcov@gmail.com>
2024-05-08 19:06:17 +0300

    what

--- dist/newone.js
+++ dist/newone.js
@@ -5,3 +5,14 @@ function autoResize() {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight + 'px';
 }
+
+//carret func ----------------
+
+const pre = document.createTextNode(textBeforeCursor);
+const post = document.createTextNode(textAfterCursor);
+const caretEle = document.createElement('span');
+caretEle.classList.add('container__cursor');
+caretEle.innerHTML = '&nbsp;';
+
+mirroredEle.innerHTML = '';
+mirroredEle.append(pre, caretEle, post);
